6b5ea90b304702c903921353ef09227c3e915cb7
Harshikesh <harshibabu604@gmail.com>
2025-02-28 11:25:58 +0530

    learnt how to use findFirst and FindAny

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -13,12 +13,20 @@ public class Lecture6 {
   @Test
   public void findAny() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+    Integer integer = Arrays.stream(numbers).filter(num-> num>5).findAny().get();
+    System.out.println("findany - "+integer);
   }
 
+  /*The differennce between findFirst and 
+   * FindAny is that FindFirst is deterministic in nat
+   * ure no matter how many times you run it you get the same output for same input.
+   * 
+   */
   @Test
   public void findFirst() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Integer integer = Arrays.stream(numbers).filter(num-> num>5).findFirst().get();
+    System.out.println("findfirst - "+integer);
 
   }
 }
