5633466d8b1d95aed32a280e8c7c2e53621aced9
Harshikesh <harshibabu604@gmail.com>
2025-03-01 17:37:28 +0530

    learnt that reduce has three param - identity, accumulator and combiner

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -1,8 +1,8 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
 import java.util.Arrays;
+
 import org.junit.Test;
 
 public class Lecture9 {
@@ -10,6 +10,8 @@ public class Lecture9 {
   @Test
   public void reduce() throws Exception {
     Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+    Integer sum = Arrays.stream(integers).reduce(0, (a, b)->(a+b));
+    System.out.println(sum);
 
   }
 
