ebf91bac2ec43726d5a3c6036e20a362c1778ea1
Harshikesh <harshibabu604@gmail.com>
2025-03-01 14:14:22 +0530

    learnt about collectors.groupingBy

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -1,40 +1,34 @@
 package lectures;
 
-
-import beans.Car;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import mockdata.MockData;
+
 import org.assertj.core.util.Lists;
 import org.junit.Test;
 
+import beans.Car;
+import mockdata.MockData;
+
 public class Lecture8 {
 
-  @Test
-  public void simpleGrouping() throws Exception {
+	@Test
+	public void simpleGrouping() throws Exception {
+		Map<String, List<Car>> groupedCarByMake = MockData.getCars().stream()
+				.collect(Collectors.groupingBy(Car::getMake));
+		
+		groupedCarByMake.forEach((make, cars) ->{
+			System.out.println(make);
+			cars.forEach(eachCar -> System.out.println(eachCar));
+		});
+	}
 
-  }
+	@Test
+	public void groupingAndCounting() throws Exception {
+		ArrayList<String> names = Lists.newArrayList("John", "John", "Mariam", "Alex", "Mohammado", "Mohammado",
+				"Vincent", "Alex", "Alex");
 
-  @Test
-  public void groupingAndCounting() throws Exception {
-    ArrayList<String> names = Lists
-        .newArrayList(
-            "John",
-            "John",
-            "Mariam",
-            "Alex",
-            "Mohammado",
-            "Mohammado",
-            "Vincent",
-            "Alex",
-            "Alex"
-        );
-
-
-  }
+	}
 
 }
