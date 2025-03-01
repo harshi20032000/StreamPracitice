10376e838b1162fde9d960170e00b8be6576f7f2
Harshikesh <harshibabu604@gmail.com>
2025-03-01 18:23:55 +0530

    List::stream is equivalent to eachList->eachList.stream()

--- src/test/java/lectures/Lecture10.java
+++ src/test/java/lectures/Lecture10.java
@@ -1,11 +1,13 @@
 package lectures;
 
-import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 public class Lecture10 {
 
@@ -25,12 +27,14 @@ public class Lecture10 {
 			list.addAll(string);
 			return list;
 		});
-		System.out.println(combinedResult);
+		System.out.println("withoutFlatmap: "+combinedResult);
 	}
 
 	@Test
 	public void withFlatMap() throws Exception {
 //   [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+		List<String> combinedList = arrayListOfNames.stream().flatMap(List::stream).collect(Collectors.toList());
+		System.out.println("withFlatmap: "+combinedList);
 
 	}
 
